516ea896d3dd3709f68e0d9df74693ce466b8def
dylanaldrich <aldrich.dylan@gmail.com>
2020-11-04 22:07:59 -0500

    adds class to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,9 @@ import ScrollUpButton from 'react-scroll-up-button';
 /* Footer Component */
 const Footer = (props) => {
     return (
-        <footer className="text-muted">
+        <footer className="text-muted footer">
             <div className="container mx-0">
                 <p className="float-right">
-                {/* <a href="#">Back to top</a> */}
                 <ScrollUpButton />
                 </p>
                 <p id='copyright'>&copy; {(new Date().getFullYear())} foodbook</p>
